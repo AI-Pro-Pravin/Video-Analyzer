af40cf6d332e6167b19e76daf1f10f9fc2018059
AI-Pro-Pravin <shettipravin@gmail.com>
2024-08-15 22:46:54 +0530

    all

--- app1.py
+++ app1.py
@@ -3,8 +3,8 @@ import whisper
 from transformers import pipeline
 import streamlit as st
 
-import ffmpeg_downloader
-ffmpeg_downloader.add_path()
+import static_ffmpeg
+static_ffmpeg.add_paths()
 
 model = whisper.load_model('base')
 pipe = pipeline(model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
@@ -13,8 +13,8 @@ pipe = pipeline(model="distilbert/distilbert-base-uncased-finetuned-sst-2-englis
 st.title("Video Analysis: Speech-to-Text and Sentiment Analysis")
 
 # File upload
-uploaded_file = st.text_input("Upload a video file path",None)
-#uploaded_file = st.file_uploader("Upload a video file",type=['mp4'])
+#uploaded_file = st.text_input("Upload a video file path",None)
+uploaded_file = st.file_uploader("Upload a video file",type=['mp4'])
 
 if uploaded_file is not None:
     # Convert video to audio
